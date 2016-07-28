8fa48491a02df8c2caf619ea595207273db7a32f
David Knaack <davidkna@users.noreply.github.com>
2016-07-28 21:54:59 +0200

    commit

--- index.js
+++ index.js
@@ -11,6 +11,38 @@ import xdg from 'xdg-basedir'
 const cloneDir = path.join(xdg.data, 'zsh_plugins')
 mkdirp(cloneDir)
 
+// POLYFILL
+
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) { // eslint-disable-line
+    const O = Object(this)
+    const len = parseInt(O.length, 10) || 0
+    if (len === 0) {
+      return false
+    }
+    const n = parseInt(arguments[1], 10) || 0 // eslint-disable-line
+    let k
+    if (n >= 0) {
+      k = n
+    } else {
+      k = len + n
+      if (k < 0) { k = 0 }
+    }
+    let currentElement
+    while (k < len) {
+      currentElement = O[k]
+      if (searchElement === currentElement ||
+         (searchElement !== searchElement && currentElement !== currentElement)) { // eslint-disable-line
+        return true
+      }
+      k++
+    }
+    return false
+  }
+}
+
+// END POLYFILL
+
 let plugins = [
   'Tarrasch/zsh-colors',
   'zsh-users/zsh-syntax-highlighting',
