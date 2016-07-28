5d0053f545b2d79ae1d1bfed54b4e68a378abcac
David Knaack <davidkna@users.noreply.github.com>
2016-07-28 21:37:53 +0200

    move things

--- test.sh
+++ test.sh
@@ -5,13 +5,13 @@ npm run test
 echo "Test with no uglify"
 npm run build:nouglify
 chmod +x dist.js
-sh cli.sh
+node cli.js
 
 echo "Test with uglify"
 rm dist.js
 npm run build
 chmod +x dist.js
-sh cli.sh
+node cli.js
 
 echo "Source file:"
 cat ~/.local/share/zsh_plugins/plugins.zsh
