cc547bda6bafed4fec4219b75572d579a2732599
David Knaack <davidkna@users.noreply.github.com>
2017-01-30 15:22:29 +0100

    toml

--- test/test.sh
+++ test/test.sh
@@ -4,10 +4,10 @@ set -e
 
 npm run test
 
-echo "Config file is at ${XDG_CONFIG_HOME:-$HOME/.config}/zsh-goggles/config.js"
+echo "Config file is at ${XDG_CONFIG_HOME:-$HOME/.config}/zsh-goggles/config.toml"
 
 mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/zsh-goggles"
-cp test/zsh-goggles.config.js "${XDG_CONFIG_HOME:-$HOME/.config}/zsh-goggles/config.js"
+cp test/zsh-goggles.config.toml "${XDG_CONFIG_HOME:-$HOME/.config}/zsh-goggles/config.toml"
 
 echo "Test if it works with no uglify"
 npm run devbuild
